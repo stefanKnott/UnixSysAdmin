943665d743142862c19dd0b02cad2c287422906a
Stefan Knott <stkn5801@colorado.edu>
2015-10-12 13:55:36 -0600

    corrections

--- Lab4/lab4.pp
+++ Lab4/lab4.pp
@@ -1,106 +1,3 @@
-file {"/home":
-	ensure => "directory",
-	owner => "mscott",
-}
-
-file {"/home/mscott":
-	ensure => "directory",
-	owner => "mscott",
-	source => "/etc/skel/"
-
-}
-
-file {"/home/dschrute":
-	ensure => "directory",
-	owner => "dschrute",
-	source => "/etc/skel/"
-
-}
-
-file {"/home/jhalpert":
-	ensure => "directory",
-	owner => "jhalpert",
-	source => "/etc/skel/"
-}
-
-file {"/home/tflenderson":
-	ensure => "directory",
-	owner => "tflenderson",
-	source => "/etc/skel/"
-}
-
-file {"/home/dphilbin":
-	ensure => "directory",
-	owner => "dphilbin",
-	source => "/etc/skel/"
-}
-
-file {"/home/amartin":
-	ensure => "directory",
-	owner => "amartin",
-	source => "/etc/skel/"
-}
-
-file {"/home/cbratton":
-	ensure => "directory",
-	owner => "cbratton",
-	source => "/etc/skel/"
-}
-
-file {"/home/kmalone":
-	ensure => "directory",
-	owner => "kmalone",
-	source => "/etc/skel/"
-}
-
-file {"/home/omartinez":
-	ensure => "directory",
-	owner => "omartinez",
-	source => "/etc/skel/"
-}
-
-file {"/home/mpalmer":
-	ensure => "directory",
-	owner => "mpalmer",
-	source => "/etc/skel/"
-}
-
-file {"/home/pbeesley":
-	ensure => "directory",
-	owner => "pbeesley",
-	source => "/etc/skel/",
-}
-
-file {"/home/kkapoor":
-	ensure => "directory",
-	owner => "kkapoor",
-	source => "/etc/skel/"
-}
-
-file {"/home/abernard":
-	ensure => "directory",
-	owner => "abernard",
-	source => "/etc/skel/"
-}
-
-file {"/home/plapin":
-	ensure => "directory",
-	owner => "plaplin",
-	source => "/etc/skel/"
-}
-
-file {"/home/shudson":
-	ensure => "directory",
-	owner => "shudon",
-	source => "/etc/skel/"
-}
-
-file {"/home/sknott":
-	ensure => "directory",
-	owner => "sknott",
-	source => "/etc/skel/"
-}
-
 group { "managers":
 	ensure => "present",
 }
@@ -111,158 +8,240 @@ group { "sales":
 
 group { "accounting": 
 	ensure => "present",
-	gid 3,
 }
 
 user{ 'mscott':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '1',
-	groups =['managers']
-	home => '/home',
+	password => 'ChangeMe',
+	groups => ['managers'],
+	home => '/home/mscott/',
 	shell => '/bin/bash',
-	uid => '1',
+	uid => '1001',
 }
 
 user{ 'dschrute':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '2',
-	groups =['managers']
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	groups => ['managers'],
+	home => '/home/dschrute/',
 	shell => '/bin/bash',
-	uid => '2',
+	uid => '1002',
 }
 
 user{ 'jhalpert':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '3',
-	groups =['managers']
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	groups => ['managers'],
+	home => '/home/jhalpert/',
 	shell => '/bin/bash',
-	uid => '3',
+	uid => '1003',
 }
 
 user{ 'tflenderson':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '4',
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	home => '/home/tflenderson/',
 	shell => '/bin/bash',
-	uid => '4',
+	uid => '1004',
 }
 
 user{ 'dphilbin':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '5',
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	home => '/home/philbin/',
 	shell => '/bin/bash',
-	uid => '5',
+	uid => '1005',
 }
 
 user{ 'amartin':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '6',
-	groups =['accounting']
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	groups => ['accounting'],
+	home => '/home/amartin/',
 	shell => '/bin/bash',
-	uid => '6',
+	uid => '1006',
 }
 
 user{ 'cbratton':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '7',
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	home => '/home/cbratton/',
 	shell => '/bin/bash',
-	uid => '7',
+	uid => '1007',
 }
 
 user{ 'kmalone':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '8',
-	groups =['accounting']
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	groups => ['accounting'],
+	home => '/home/kmalone/',
 	shell => '/bin/bash',
-	uid => '8',
+	uid => '1008',
 }
 
 user{ 'omartinez':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '9',
-	groups =['accounting']
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	groups => ['accounting'],
+	home => '/home/omartinez/',
 	shell => '/bin/bash',
-	uid => '9',
+	uid => '1009',
 }
 
 user{ 'mpalmer':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '10',
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	home => '/home/mpalmer/',
 	shell => '/bin/bash',
-	uid => '10',
+	uid => '1010',
 }
 
 user{ 'pbeesly':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '11',
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	home => '/home/pbeesly/',
 	shell => '/bin/bash',
-	uid => '11',
+	uid => '1011',
 }
 
 user{ 'kkapoor':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '12',
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	home => '/home/kkapoor/',
 	shell => '/bin/bash',
-	uid => '12',
+	uid => '1012',
 }
 
 user{ 'abernard':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '13',
-	groups =['sales']
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	groups => ['sales'],
+	home => '/home/abernard/',
 	shell => '/bin/bash',
-	uid => '13',
+	uid => '1013',
 }
 
 user{ 'plapin':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '14',
-	groups =['sales']
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	groups => ['sales'],
+	home => '/home/plaplin/',
 	shell => '/bin/bash',
-	uid => '14',
+	uid => '1014',
 }
 
 user{ 'shudson':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '15',
-	groups =['sales']
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	groups => ['sales'],
+	home => '/home/shudson/',
 	shell => '/bin/bash',
-	uid => '15',
+	uid => '1015',
 }
 
 user{ 'sknott':
 	ensure => 'present',
-	password => 'ChangeMe'
-	gid => '16',
-	home => 'path/to/home',
+	password => 'ChangeMe',
+	home => '/home/',
 	shell => '/bin/bash',
-	uid => '16',
+	uid => '1016',
+}
+
+
+file {"/home/mscott":
+	ensure => "directory",
+	owner => "mscott",
+	source => "~/../etc/skel/"
+
+}
+
+file {"/home/dschrute":
+	ensure => "directory",
+	owner => "dschrute",
+	source => "~/../etc/skel/"
+
+}
+
+file {"/home/jhalpert":
+	ensure => "directory",
+	owner => "jhalpert",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/tflenderson":
+	ensure => "directory",
+	owner => "tflenderson",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/dphilbin":
+	ensure => "directory",
+	owner => "dphilbin",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/amartin":
+	ensure => "directory",
+	owner => "amartin",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/cbratton":
+	ensure => "directory",
+	owner => "cbratton",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/kmalone":
+	ensure => "directory",
+	owner => "kmalone",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/omartinez":
+	ensure => "directory",
+	owner => "omartinez",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/mpalmer":
+	ensure => "directory",
+	owner => "mpalmer",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/pbeesley":
+	ensure => "directory",
+	owner => "pbeesly",
+	source => "~/../etc/skel/",
+}
+
+file {"/home/kkapoor":
+	ensure => "directory",
+	owner => "kkapoor",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/abernard":
+	ensure => "directory",
+	owner => "abernard",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/plapin":
+	ensure => "directory",
+	owner => "plapin",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/shudson":
+	ensure => "directory",
+	owner => "shudson",
+	source => "~/../etc/skel/"
+}
+
+file {"/home/sknott":
+	ensure => "directory",
+	owner => "sknott",
+	source => "~/../etc/skel/"
 }
